26703bc4c99f62b58bb8871aaccbd367bb41b402
laveeshsanadhya <laveeshsanadhya@gmail.com>
2025-06-22 00:00:54 +0530

    error middle ware completed

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 import express from 'express';
 import {PORT} from './config/env.js' ;
 import connectionToDatabase from './database/mongodb.js';
-
+import cookieParser from 'cookie-parser';
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 // import { connection } from 'mongoose';
 
 const app = express() ; 
+app.use(express.json()) ; 
+app.use(express.urlencoded({ extended: false })) ;
+app.use(cookieParser()) ;  
 
 app.use('/api/v1/auth' , authRouter) ; 
 app.use('/api/v1/users' , userRouter) ;
 app.use('/api/v1/subscriptions' , subscriptionRouter) ;
+
+app.use(errorMiddleware) ; // Error handling middleware
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
